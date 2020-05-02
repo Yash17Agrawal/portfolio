7139c219cd7928e583ef979e5f802d4ebe78c7bc
Yash <yash17agrawal@gmail.com>
2020-05-02 22:53:20 +0530

    Updates

---
+++ precache-manifest.65e6db7c5bbf704fb015e949d02604df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0739f8a39115db970315e0a23aa64e32",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6d8829c6a5e723283c83",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2fc6ed07ac87f9a1f84e",
+    "url": "/portfolio/static/js/2.0ba1d00e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.0ba1d00e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d8829c6a5e723283c83",
+    "url": "/portfolio/static/js/main.b9459b50.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  }
+]);
