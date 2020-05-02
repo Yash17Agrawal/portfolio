1b6d84b0a02bc5ecd910da0ead9708cdec60b3f2
Yash <yash17agrawal@gmail.com>
2020-05-02 22:49:00 +0530

    Added Dashboard and drawer components

---
+++ src/constants/common.ts
@@ -0,0 +1,7 @@
+export const MEDIA_URLS = {
+  TWITTER: "https://twitter.com/scorncer17",
+  LINKEDIN: "https://www.linkedin.com/in/yash-agrawal-aa436135/",
+  GITHUB: "https://github.com/Yash17Agrawal/",
+  STACKOVERFLOW: "https://stackoverflow.com/users/6627786/yash-agrawal",
+  MEDIUM: "https://medium.com/@yash17ag"
+}
---
+++ src/constants/messages.ts
@@ -0,0 +1,7 @@
+export const messages = {
+  "drawer.listItem1": "Home",
+  "drawer.listItem2": "Resume",
+  "drawer.listItem3": "Projects",
+  "drawer.listItem4": "Tech stack",
+  "drawer.listItem5": "Contact",
+}
