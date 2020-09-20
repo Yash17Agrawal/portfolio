2a6061913199f03d3242976b7c004deaaca392f6
Yash <yash17agrawal@gmail.com>
2020-09-20 11:53:28 +0530

    Updates

---
+++ precache-manifest.e4242129586b84786ff3a23dde2b8539.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883e2372f6bf6d294e1413241b4f4477",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "13adbeb5e871596c775d",
+    "url": "/portfolio/static/css/main.5663a14e.chunk.css"
+  },
+  {
+    "revision": "f82c46fd00329992b019",
+    "url": "/portfolio/static/js/2.47c681c3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.47c681c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13adbeb5e871596c775d",
+    "url": "/portfolio/static/js/main.1a3ffc71.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "02d040a115d3de6b850bee387d2e2e44",
+    "url": "/portfolio/static/media/book.02d040a1.jpg"
+  },
+  {
+    "revision": "39cb8d476a56420edbee3f347ac6e3b5",
+    "url": "/portfolio/static/media/profilephoto.39cb8d47.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.65e6db7c5bbf704fb015e949d02604df.js"
+  "/portfolio/precache-manifest.e4242129586b84786ff3a23dde2b8539.js"
 );
 
 self.addEventListener('message', (event) => {
